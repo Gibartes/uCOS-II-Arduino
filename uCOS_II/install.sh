6d5cb82b71de0bbd9cd1adf7ec1a14524ec17b3c
Gibartes <konm2000@hotmail.com>
2017-08-14 01:49:33 +0900

    Update install.sh

--- uCOS_II/install.sh
+++ uCOS_II/install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+LPATH=~/Arduino/libraries/uCOS_II
 echo -e "Installing uCOS-II libraries..."
-if [ -d ~/Arduino/libraries/uCOS_II ];then
+if [ -d ${LPATH} ];then
 	read -p  "Are you sure to overwrite uCOS_II? [y/n]" yn
 	case $yn in
 		[Yy]* ) 
-			cp -rf * ~/Arduino/libraries/uCOS_II
+			cp -rf * ${LPATH}
 			if [ $? -gt 0 ];then
 				echo -e "Failed to install uCOS_II library for Arduino."
 				exit 1
@@ -14,8 +15,8 @@ if [ -d ~/Arduino/libraries/uCOS_II ];then
 	esac
 	exit 0
 fi
-mkdir -p ~/Arduino/libraries/uCOS_II
-cp -rf * ~/Arduino/libraries/uCOS_II
+mkdir -p ${LPATH}
+cp -rf * ${LPATH}
 if [ $? -gt 0 ];then
 	echo -e "Failed to install uCOS_II library for Arduino."
 	exit 1
